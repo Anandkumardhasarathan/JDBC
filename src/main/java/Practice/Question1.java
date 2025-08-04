7556f2ad8413d41426061f6ba3e6379989ed19fb
Anandkumardhasarathan <anandhakumarak75@gmail.com>
2025-08-04 12:52:33 +0530

    dynamic input

--- src/main/java/Practice/Question1.java
+++ src/main/java/Practice/Question1.java
@@ -1,12 +1,13 @@
 package Practice;
 
 import java.sql.*;
+import java.util.Scanner;
 public class Question1 {
 
 	public static void main(String[] args) throws SQLException{
 //		create();
-		read();
-
+//		read();
+		insertrt1();
 	}
 	
 	public static void create() throws SQLException{
@@ -30,6 +31,35 @@ public class Question1 {
 		ResultSet rs = s.executeQuery(q);
 		while(rs.next()) {
 			System.out.println(rs.getInt(1)+" | "+rs.getString(2)+" \t\t | "+ rs.getString(3));
+		}
+		
+		
+	}
+	
+	public static void insertrt1() throws SQLException{
+		Scanner scan = new Scanner(System.in);
+		String url = "jdbc:mysql://localhost:3306/placement_training";
+		String un = "root";
+		String pw = "7547";
+		Connection c = DriverManager.getConnection(url, un, pw);
+		Statement s = c.createStatement();
+		
+		System.out.print("Enter the Number of values to be insert : ");
+		int n = scan.nextInt();
+		
+		for(int k = 1;k<=n;k++) {
+			
+			int val1 = scan.nextInt();
+			scan.nextLine();
+			
+			String val2 = scan.nextLine();
+			scan.nextLine();
+			
+			String val3 = scan.nextLine();
+			scan.nextLine();
+			
+			String q = "insert into task values("+val1+",'"+val2+"','"+val3+"');";
+			s.executeUpdate(q);
 		}
 	}
 
